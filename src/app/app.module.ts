fade54b4df8a20ff2108e7c54bd31e2aa52f7437
blkopsdev <blkopsdev@gmail.com>
2018-07-08 08:14:21 -0400

    Added index, create, edit components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
