7a45bb0f17adba741c875c926714cd5e3490f379
blkopsdev <blkopsdev@gmail.com>
2018-07-08 11:32:45 -0400

    modified:   ../app.component.html 	modified:   ../app.component.ts 	modified:   ../app.module.ts 	modified:   create/create.component.html 	modified:   create/create.component.ts 	modified:   ../../index.html 	../../assets/css/ 	../../assets/js/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+// app.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Welcome to crypto world';
 }
